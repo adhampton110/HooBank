a79efdc5473b2f13890f307b16d8581a7337bd98
adhampton110 <anthony.hampton1235@gmail.com>
2023-07-26 23:03:41 -0500

    Deal Layout

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   import Hero from './layouts/Hero/Hero.vue';
   import Business from './layouts/Business/Business.vue';
   import Control from './layouts/Control/Control.vue';
+  import Deal from './layouts/Deal/Deal.vue';
 </script>
 
 <template>
   <Hero/>
   <Business/>
   <Control/>
+  <Deal/>
 </template>
 
 <style scoped>
---
+++ src/layouts/Deal/Deal.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import CTARectangle from '../../components/CTARectangle.vue';
+</script>
+<template>
+    <section id="deal">
+        <div class="deal-content">
+            <h2>Find a better card deal in few easy steps.</h2>
+            <p>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+            <CTARectangle/>
+        </div>
+        <img class="deal-image" src="../../assets/card.png" alt="card">
+    </section>
+</template>
+
+<style scoped>
+    #deal {
+        display: flex;
+        justify-content: space-between;
+    }
+    .deal-content {
+        max-width: 570px;
+    }
+    p {
+        margin-top: 24px;
+        margin-bottom: 48px;
+    }
+    .deal-image {
+        width: 513px;
+        height: 449px;
+    }
+</style>
